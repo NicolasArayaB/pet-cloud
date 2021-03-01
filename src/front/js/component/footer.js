4bdd57015b63edfeb927964b9e484ad264f34e43
NicolasArayaB <nicolasaraya@gmail.com>
2021-03-01 21:20:05 +0000

    simple footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<Link to="/" className="footerLink">
+			Home
+		</Link>
+		<span className="footerLink">|</span>
+		<Link to="/" className="footerLink">
+			Servicios
+		</Link>
+		<span className="footerLink">|</span>
+		<Link to="/" className="footerLink">
+			Contacto
+		</Link>
 	</footer>
 );
