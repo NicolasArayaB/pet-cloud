aa0cc233a3eb037a9946607b5ad690f4094e0fc4
NicolasArayaB <nicolasaraya@gmail.com>
2021-03-05 14:37:25 +0000

    login modal

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Container, Row, Col } from "react-bootstrap";
+import { Navbar, Container, Row, Col, Button } from "react-bootstrap";
 
 import HomeNavInfo from "./homeNavInfo";
-import HomeNavLogin from "./homeNavLogin";
+import LoginModal from "./homeNavLogin";
 
 export const MyNavbar = () => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	return (
-		<Navbar collapseOnSelect expand="lg" className="petCloudBar" variant="light">
-			<Navbar.Brand href="#home">
-				<Link to="/">
-					<img
-						className="logo"
-						src="https://raw.githubusercontent.com/NicolasArayaB/pet-cloud/navbar/src/front/img/petcloudlogo.png"
-					/>
-				</Link>
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Container fluid className="navContainer">
-					<Row>
-						<Col sm={10} className="d-flex justify-content-center">
-							<HomeNavInfo />
-						</Col>
-						<Col sm={2}>
-							<HomeNavLogin />
-						</Col>
-					</Row>
-				</Container>
-			</Navbar.Collapse>
-		</Navbar>
+		<>
+			<LoginModal show={show} close={handleClose} />
+			<Navbar collapseOnSelect expand="lg" className="petCloudBar" variant="light">
+				<Navbar.Brand href="#home">
+					<Link to="/">
+						<img
+							className="logo"
+							src="https://raw.githubusercontent.com/NicolasArayaB/pet-cloud/navbar/src/front/img/petcloudlogo.png"
+						/>
+					</Link>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Container fluid className="navContainer">
+						<Row>
+							<Col sm={10} className="d-flex justify-content-center">
+								<HomeNavInfo />
+							</Col>
+							<Col sm={2}>
+								<Button onClick={handleShow} className="mx-4 btn login">
+									Login
+								</Button>
+							</Col>
+						</Row>
+					</Container>
+				</Navbar.Collapse>
+			</Navbar>
+		</>
 	);
 };
