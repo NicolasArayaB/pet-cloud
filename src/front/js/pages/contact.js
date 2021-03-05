7618948c53ed0e86ae764de03927a7252a7d9c50
francisco-correa <fcorrea.fernandez@gmail.com>
2021-03-05 17:13:23 +0000

    update contact view with validation

--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 export const Contact = () => {
+	const [validated, setValidated] = useState(false);
+
+	const handleSubmit = event => {
+		const form = event.currentTarget;
+		if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+
+		setValidated(true);
+	};
 	return (
 		<Container>
-			<Row className="my-5 mx-5 mr-5">
+			<Row className="justify-content-center">
 				<Col xs="12" md="6">
-					<Form>
+					<Form noValidate validated={validated} onSubmit={handleSubmit}>
 						<Form.Group controlId="exampleForm.ControlSelect1">
-							<Form.Label style={{ marginTop: "20px" }}>Selecciona tu perfil</Form.Label>
+							<Form.Label style={{ marginTop: "40px" }}>Selecciona tu perfil</Form.Label>
 							<Form.Control as="select">
 								<option>Soy Veterinario</option>
 								<option>Soy Dueño de mascota</option>
 							</Form.Control>
 						</Form.Group>
 						<Form.Group controlId="validationCustom02">
-							<Form.Label>Tu Nombre y Apellido</Form.Label>
-							<Form.Control required type="text" placeholder="Nombre y Apellido" />
+							<Form.Control required type="text" placeholder="Ingresa tu Nombre y Apellido" />
+							<Form.Control.Feedback>Se ve bien eso!</Form.Control.Feedback>
 						</Form.Group>
 						<Form.Group controlId="exampleForm.ControlInput1">
-							<Form.Label>Tu Email</Form.Label>
-							<Form.Control type="email" placeholder="nombre@ejemplo.com" required />
+							<Form.Control type="email" placeholder="Ingresa tu mail: nombre@ejemplo.com" required />
+							<Form.Control.Feedback>Se ve bien eso!</Form.Control.Feedback>
 						</Form.Group>
 						<Form.Group controlId="exampleForm.ControlTextarea1">
 							<Form.Label>Cuentanos de Tu problema</Form.Label>
