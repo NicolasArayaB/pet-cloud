6f2b4379325f4d47eb021a3ce8328b400093ffba
francisco-correa <fcorrea.fernandez@gmail.com>
2021-03-14 02:12:15 +0000

    pet information layout

--- src/front/js/component/userFiles.js
+++ src/front/js/component/userFiles.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Accordion, Card, Button } from "react-bootstrap";
+import { Accordion, Card, Button, Link } from "react-bootstrap";
 
 const UserFiles = () => {
 	return (
