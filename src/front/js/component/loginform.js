daf61db4c1814fef3d44c0f5f8a31fab72a2dde6
NicolasArayaB <nicolasaraya@gmail.com>
2021-03-15 00:34:55 +0000

    modal link to forgottenPass available and resized input

--- src/front/js/component/loginform.js
+++ src/front/js/component/loginform.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const LoginForm = () => {
+const LoginForm = params => {
 	return (
 		<Form autocomplete="off">
 			<Form.Control type="email" placeholder="Ingresa tu e-mail" className="mb-2" />
@@ -10,7 +10,9 @@ const LoginForm = () => {
 				<Form.Control type="password" placeholder="Ingresa tu contraseña" />
 				<Form.Text>La contraseña debe tener entre 6 a 12 characteres</Form.Text>
 			</Form.Group>
-			<Link>¿Olvidaste tu contraseña?</Link>
+			<Link to="/pass" onClick={params.close}>
+				¿Olvidaste tu contraseña?
+			</Link>
 		</Form>
 	);
 };
--- src/front/js/pages/forgottenPassword.js
+++ src/front/js/pages/forgottenPassword.js
@@ -3,7 +3,7 @@ import { Container, Form, Button } from "react-bootstrap";
 
 export const ForgottenPass = () => {
 	return (
-		<Container className="registerForms">
+		<Container className="registerForms forgottenPass">
 			<Form autocomplete="off" className="p-5 text-center">
 				<Form.Text className="m-3">Ingresa tu email para recuperar tu contraseña</Form.Text>
 				<Form.Control type="email" placeholder="tuemail@petcloud.com" className="m-3 text-center" />
