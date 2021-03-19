8c5cb81d73ca08bb18906f35ea48f55a406f1f87
NicolasArayaB <nicolasaraya@gmail.com>
2021-03-19 20:12:53 +0000

    working database

--- src/api/models.py
+++ src/api/models.py
@@ -1,40 +1,62 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 # starting tables with users (organization, veterinary and user)
 
 class User(db.Model):
-    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    vet_user_fk = Column(Integer, ForeignKey('vet_user.id'))
+    vet_user_fk = db.Column(db.Integer, db.ForeignKey('vet_user.id'))
+    vet_users = relationship("Vet_user", foreign_keys=[vet_user_fk])
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
 
-class Veterinary_organization(db.Model):
-    __tablename__ = 'veterinary_organization'
-    id = db.Column(Integer, primary_key=True)
-    name = db.Column(db.String(100)
-    alias_name = db.Column(db.String(100)
-    city = db.Column(db.String(100)
-    address = db.Column(db.String(100)
-    city = db.Column(db.String(100)
-    phone = db.Column(db.Integer)
-    email = db.Column(db.String(80), unique=True)
-    vet_user_fk = Column(Integer, ForeignKey('vet_user.id'))
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
 
 class Vet_user(db.Model):
     __tablename__ = 'vet_user'
-    id = Column(Integer, primary_key=True)
-    RUN = db.Column(db.String(12)
-    first_name = db.Column(db.String(50)
-    last_name = db.Column(db.String(50)
-    gender = db.Column(db.String(50)
-    birth_date = db.Column(db.String(50)
+    id = db.Column(db.Integer, primary_key=True)
+    RUN = db.Column(db.String(12))
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    gender = db.Column(db.String(50))
+    birth_date = db.Column(db.String(50))
     is_exotic = db.Column(db.Boolean)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_fk = db.Column(db.Integer, db.ForeignKey('user.id'))
+    users = relationship('User', foreign_keys=[user_fk])
 
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Veterinary_organization(db.Model):
+    __tablename__ = 'veterinary_organization'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    alias_name = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    address = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    phone = db.Column(db.Integer)
+    email = db.Column(db.String(80), unique=True)
+    vet_user_fk = db.Column(db.Integer, db.ForeignKey('vet_user.id'))
+    vet_users = relationship("Vet_user", foreign_keys=[vet_user_fk])
 
     def __repr__(self):
         return '<User %r>' % self.username
--- src/api/routes.py
+++ src/api/routes.py
@@ -19,48 +19,48 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@app.route("/user", methods=["POST"])
-def create_user():
-    data = request.get_json()
-    hashed_password = generate_password_hash(data["password"], method="sha256")
-    new_user = User(id=data["id"], name=data["name"], password=hashed_password)
-    db.session.add(new_user) 
-    db.session.commit() 
-    return jsonify({"Message": "New user has created"})
-
-@app.route("/user", methods=["GET"])
-def get_all_users():
-    users = User.query.all()
-    output = []
-    for user in users:
-        user_data = { }
-        user_data["id"] = user.id
-        user_data["name"] = user.name
-        user_data["password"] = user.password
-        output.append(user_data)
-    return jsonify({"Users": output})
-
-@app.route("/user/<id>", methods=["GET"])
-def get_one_user(id):
-    user = User.query.filter_by(id=id).first()
-    if not user:
-        return jsonify({"message": "Not user found"})
-    user_data = {}
-    user_data["id"] = user.id
-    user_data["name"] = user.name
-    user_data["password"] = user.password
-
-    return jsonify({"message": user_data})
-
-
-@app.route("/user/<id>", methods=["DELETE"])
-def delete_one_user(id):
-    user = User.query.filter_by(id=id).first()
-    if not user:
-        return jsonify({"message": "Not user found"})
-    db.session.delete(user)
-    db.session.commit()
-
-    return jsonify({"Message": "User has deleted"})
-
-# db.create_all()
+#@app.route("/user", methods=["POST"])
+#def create_user():
+#    data = request.get_json()
+#    hashed_password = generate_password_hash(data["password"], method="sha256")
+#    new_user = User(id=data["id"], name=data["name"], password=hashed_password)
+#    db.session.add(new_user) 
+#    db.session.commit() 
+#    return jsonify({"Message": "New user has created"})
+#
+#@app.route("/user", methods=["GET"])
+#def get_all_users():
+#    users = User.query.all()
+#    output = []
+#    for user in users:
+#        user_data = { }
+#        user_data["id"] = user.id
+#        user_data["name"] = user.name
+#        user_data["password"] = user.password
+#        output.append(user_data)
+#    return jsonify({"Users": output})
+#
+#@app.route("/user/<id>", methods=["GET"])
+#def get_one_user(id):
+#    user = User.query.filter_by(id=id).first()
+#    if not user:
+#        return jsonify({"message": "Not user found"})
+#    user_data = {}
+#    user_data["id"] = user.id
+#    user_data["name"] = user.name
+#    user_data["password"] = user.password
+#
+#    return jsonify({"message": user_data})
+#
+#
+#@app.route("/user/<id>", methods=["DELETE"])
+#def delete_one_user(id):
+#    user = User.query.filter_by(id=id).first()
+#    if not user:
+#        return jsonify({"message": "Not user found"})
+#    db.session.delete(user)
+#    db.session.commit()
+#
+#    return jsonify({"Message": "User has deleted"})
+#
+## db.create_all()
