0a610b127dccd7442cbe1a98f749811161bbac91
francisco-correa <fcorrea.fernandez@gmail.com>
2021-03-20 04:34:48 +0000

    continue to develop data model

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Vet_user, Veterinary_organization
+from .models import db, User, User_contact, Vet_user, Veterinary_organization, Pet, Pet_controls
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -11,8 +11,11 @@ def setup_admin(app):
 
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(User_contact, db.session))
     admin.add_view(ModelView(Vet_user, db.session))
     admin.add_view(ModelView(Veterinary_organization, db.session))
+    admin.add_view(ModelView(Pet, db.session))
+    admin.add_view(ModelView(Pet_controls, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,16 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 db = SQLAlchemy()
 
-# starting tables with users (organization, veterinary and user)
+# Data Model: 
+# user, user_contact, vet_user, veterinary_orgnization, pet, pet_controls
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    vet_user_fk = db.Column(db.Integer, db.ForeignKey('vet_user.id'))
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
+    is_active = Column(Boolean(), unique=False, nullable=False)
+    vet_user_fk = Column(Integer, ForeignKey('vet_user.id'))
     vet_users = relationship("Vet_user", foreign_keys=[vet_user_fk])
     
     def __repr__(self):
@@ -23,20 +25,40 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
+
+class User_contact(db.Model):
+    id = Column(Integer, primary_key=True)
+    address = Column(String(100))
+    city = Column(String(100))
+    phone = Column(Integer)
+    user_contact_fk = Column(Integer, ForeignKey('user.id'))
+    users_contact = relationship("User", foreign_keys=[user_contact_fk])
+    
+    def __repr__(self):
+        return '<User_contact %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "address": self.address,
+            # do not serialize the password, its a security breach
+        }
+
 class Vet_user(db.Model):
     __tablename__ = 'vet_user'
-    id = db.Column(db.Integer, primary_key=True)
-    RUN = db.Column(db.String(12))
-    first_name = db.Column(db.String(50))
-    last_name = db.Column(db.String(50))
-    gender = db.Column(db.String(50))
-    birth_date = db.Column(db.String(50))
-    is_exotic = db.Column(db.Boolean)
-    user_fk = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    RUN = Column(String(12))
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    gender = Column(String(50))
+    birth_date = Column(String(50))
+    is_exotic = Column(Boolean)
+    user_fk = Column(Integer, ForeignKey('user.id'))
     users = relationship('User', foreign_keys=[user_fk])
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Vet_user %r>' % self.username
 
     def serialize(self):
         return {
@@ -47,15 +69,14 @@ class Vet_user(db.Model):
 
 class Veterinary_organization(db.Model):
     __tablename__ = 'veterinary_organization'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    alias_name = db.Column(db.String(100))
-    city = db.Column(db.String(100))
-    address = db.Column(db.String(100))
-    city = db.Column(db.String(100))
-    phone = db.Column(db.Integer)
-    email = db.Column(db.String(80), unique=True)
-    vet_user_fk = db.Column(db.Integer, db.ForeignKey('vet_user.id'))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    alias_name = Column(String(100))
+    address = Column(String(100))
+    city = Column(String(100))
+    phone = Column(Integer)
+    email = Column(String(80), unique=True)
+    vet_user_fk = Column(Integer, ForeignKey('vet_user.id'))
     vet_users = relationship("Vet_user", foreign_keys=[vet_user_fk])
 
     def __repr__(self):
@@ -67,3 +88,45 @@ class Veterinary_organization(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Pet(db.Model):
+    __tablename__ = 'pet'
+    id = db.Column(Integer, primary_key=True)
+    chip_identifier = Column(String(50))
+    nickname = Column(String(100))
+    species = Column(String(100))
+    breed = Column(String(100))
+    gender = Column(String(100))
+    pet_user_fk = Column(Integer, ForeignKey('user.id'))
+    pet_users = relationship("User", foreign_keys=[pet_user_fk])
+
+    def __repr__(self):
+        return '<Pet %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nickname": self.nickname,
+            # do not serialize the password, its a security breach
+        }
+
+class Pet_controls(db.Model):
+    __tablename__ = 'pet_controls'
+    id = db.Column(Integer, primary_key=True)
+    pet_medication = Column(String(50))
+    pet_exams = Column(String(100))
+    pet_vaccines = Column(String(100))
+    pet_controls_fk = Column(Integer, ForeignKey('pet.id'))
+    pets = relationship("Pet", foreign_keys=[pet_controls_fk])
+
+    def __repr__(self):
+        return '<Pet_controls %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "vaccines": self.vaccines,
+            # do not serialize the password, its a security breach
+        }
+
+# db.create_all()
