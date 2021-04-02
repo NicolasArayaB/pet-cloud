34725948af2cac3080ae4cbf2ee36d4389840c9d
CablCode <acbrante@gmail.com>
2021-04-02 00:40:40 +0000

    modifican imagenes

--- src/front/js/component/pet-owner/userPhoto.js
+++ src/front/js/component/pet-owner/userPhoto.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { Card, CardGroup } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
+import "../../../styles/user.scss";
 
 const UserPhoto = () => {
 	return (
 		<div>
 			<CardGroup>
-				<Card>
+				<Card border="white">
 					<Card.Img
+						className="img-pet"
 						variant="top"
 						src="https://raw.githubusercontent.com/NicolasArayaB/pet-cloud/3.4_User_View/src/front/img/DogPhoto01.png"
+						style={{ width: "12rem" }}
 					/>
 					<Card.Body />
 				</Card>
-				<Card>
+				<Card border="white">
 					<Card.Img
+						className="img-pet"
 						variant="top"
 						src="https://raw.githubusercontent.com/NicolasArayaB/pet-cloud/3.4_User_View/src/front/img/DogPhoto02.png"
 						style={{ width: "12rem" }}
