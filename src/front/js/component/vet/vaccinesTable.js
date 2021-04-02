7ac456cbb5a730442534f8c8729f599500ef2ed0
NicolasArayaB <nicolasaraya@gmail.com>
2021-04-02 17:08:52 +0000

    vet information with api data

--- src/front/js/component/vet/vaccinesTable.js
+++ src/front/js/component/vet/vaccinesTable.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Table } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-const Vaccines = () => {
+const Vaccines = props => {
 	return (
 		<Table bordered hover>
 			<thead className="table-secondary">
@@ -13,22 +14,18 @@ const Vaccines = () => {
 			</thead>
 			<tbody>
 				<tr>
-					<th>Antirrábica</th>
-					<th>27/03/20</th>
+					<th>{props.vaccine}</th>
+					<th>{props.date}</th>
 					<th>2/3</th>
-				</tr>
-				<tr>
-					<th>Leptospirosis</th>
-					<th>20/01/20</th>
-					<th>1/1</th>
-				</tr>
-				<tr>
-					<th>Desparasitante</th>
-					<th>05/11/19</th>
-					<th>3/3</th>
 				</tr>
 			</tbody>
 		</Table>
 	);
 };
+
+Vaccines.propTypes = {
+	vaccine: PropTypes.string,
+	date: PropTypes.string
+};
+
 export default Vaccines;
