21377f9bb6409edf7ac63471698a15b7c5276f9f
NicolasArayaB <nicolasaraya@gmail.com>
2021-04-14 02:24:55 +0000

    options to responsive radios

--- migrations/versions/8c1e01607d32_.py
+++ migrations/versions/8c1e01607d32_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ef5d740d3445
+Revision ID: 8c1e01607d32
 Revises: 
-Create Date: 2021-04-13 01:17:55.592500
+Create Date: 2021-04-13 23:46:57.112502
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ef5d740d3445'
+revision = '8c1e01607d32'
 down_revision = None
 branch_labels = None
 depends_on = None
