0437ba1cb0693055a9dc53ee4e3792d042380462
CablCode <acbrante@gmail.com>
2021-04-17 15:14:34 +0000

    services responsive

--- src/front/js/component/services/cardServices.js
+++ src/front/js/component/services/cardServices.js
@@ -7,30 +7,30 @@ const CardInfo = () => {
 	return (
 		<Container>
 			<Row>
-				<Col>
+				<Col sm>
 					<img
 						src="https://raw.githubusercontent.com/NicolasArayaB/pet-cloud/15-create-services-view/src/front/img/Services1.png"
 						style={{ width: "5rem" }}
 					/>
-					<p className="mt-4" style={{ color: "grey" }}>
+					<p className="mt-2 p-4" style={{ color: "grey" }}>
 						Identifica a tus mascotas rápidamente en tu cuenta
 					</p>
 				</Col>
-				<Col>
+				<Col sm>
 					<img
 						src="https://raw.githubusercontent.com/NicolasArayaB/pet-cloud/15-create-services-view/src/front/img/Services2.png"
 						style={{ width: "5rem" }}
 					/>
-					<p className="mt-4" style={{ color: "grey" }}>
+					<p className="mt-2 p-4" style={{ color: "grey" }}>
 						Próximamente: Obten el historial de tu mascota a un click
 					</p>
 				</Col>
-				<Col>
+				<Col sm>
 					<img
 						src="https://raw.githubusercontent.com/NicolasArayaB/pet-cloud/15-create-services-view/src/front/img/Services3.png"
 						style={{ width: "5rem" }}
 					/>
-					<p className="mt-4" style={{ color: "grey" }}>
+					<p className="mt-2 p-4" style={{ color: "grey" }}>
 						Próximamente: Recibe notificaciones de los próximos controles de tu mascota
 					</p>
 				</Col>
