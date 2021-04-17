18cdd25f3017d2d4683cf1ef50cd67b694c19ce6
CablCode <acbrante@gmail.com>
2021-04-17 16:49:32 +0000

    pet not registered responsive

--- migrations/versions/84266ce1b6a7_.py
+++ migrations/versions/84266ce1b6a7_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: c3ba71930b2a
+Revision ID: 84266ce1b6a7
 Revises: 
-Create Date: 2021-04-15 00:54:02.034392
+Create Date: 2021-04-17 15:52:46.818684
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c3ba71930b2a'
+revision = '84266ce1b6a7'
 down_revision = None
 branch_labels = None
 depends_on = None
